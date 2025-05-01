776f7c1a00b3a2443971af83e5891e8ae3b54321
Adin Richter <47506271+adinrichter@users.noreply.github.com>
2025-05-01 10:30:34 -0700

    import local project files

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+zip -r repump_0.1.0.zip repump_0.1.0
+
+mv repump_0.1.0.zip "/Users/adinrichter/Library/Application Support/factorio/mods/"
---
+++ repump_0.1.0/data.lua
@@ -0,0 +1,25 @@
+--data.lua
+
+data.raw["utility-constants"]["default"].default_pipeline_extent = 80
+
+local electricPump = table.deepcopy(data.raw["pump"]["pump"])
+
+electricPump.name = "electric-pump"
+electricPump.pumping_speed = 2400
+electricPump.energy_usage = "600kW"
+
+local recipe = {
+  type = "recipe",
+  name = "electric-pump",
+  enabled = true,
+  energy_required = 6,
+  ingredients = {
+    {type = "item", name = "electric-engine-unit", amount = 2},
+    {type = "item", name = "pipe", amount = 4},
+    {type = "item", name = "steel-plate", amount = 4},
+    {type = "item", name = "advanced-circuit", amount = 2}
+},
+  results = {{type = "item", name = "electric-pump", amount = 1}}
+}
+
+data:extend{electricPump, recipe}
